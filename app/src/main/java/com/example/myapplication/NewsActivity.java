ace68f2fbcf35ce229d6968c6861c4b1c904d5fb
Binhcnttk14a <49368272+Binhcnttk14a@users.noreply.github.com>
2019-04-11 19:33:27 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/NewsActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class NewsActivity<Webview> extends AppCompatActivity {
+
+    WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        webview = (WebView) findViewById(R.id.webviewTinTuc);
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("linkTinTuc");
+        webview.loadUrl(link);
+        webview.setWebViewClient(new WebViewClient());
+   }
+}
